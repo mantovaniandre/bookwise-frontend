6a785951e6e7b821060bbda1830934cd341d8038
mantovaniandre <andreluizdiasmantovani@gmail.com>
2023-04-18 21:31:43 -0300

    Atualização

--- bookwise/src/app/utils/service/app-routing.service.ts
+++ bookwise/src/app/utils/service/app-routing.service.ts
@@ -4,10 +4,12 @@ import { LoginComponent } from '../../login/login.component';
 import { UserRegisterComponent } from '../../user-register/user-register.component';
 import { HomeComponent } from '../../home/home.component';
 import { AuthGuard } from '../guard/auth.guard';
+import { LearnMoreComponent } from 'src/app/learn-more/learn-more.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/login', pathMatch: 'full' },
   { path: 'register', component: UserRegisterComponent },
+  { path: 'learnMore', component: LearnMoreComponent },
   { path: 'login', component: LoginComponent },
   { path: 'home', component: HomeComponent},
   { path: '**', redirectTo: '/home' }
