aff87d75496500e66a17519a2dbc0176ce5c997d
mantovaniandre <andreluizdiasmantovani@gmail.com>
2023-04-23 22:25:49 -0300

    Atualização

--- bookwise/src/app/login/login.component.ts
+++ bookwise/src/app/login/login.component.ts
@@ -1,10 +1,5 @@
-import { FormBuilder, Validators } from '@angular/forms';
-import { AuthService } from '../utils/service/auth.service';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 import { Component} from '@angular/core';
-import { HttpErrorResponse } from '@angular/common/http';
-import { LoginRequest } from '../utils/request/login.request';
-import { BehaviorSubject, Observable, timer } from 'rxjs';
-import { Router } from '@angular/router';
 
 
 @Component({
@@ -13,52 +8,29 @@ import { Router } from '@angular/router';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent {
-  private tokenSubject = new BehaviorSubject<string>('');
-  token$: Observable<string> = this.tokenSubject.asObservable();
-  disabledButton = true;
   
-  loginFormGroup = this.fb.group({
-    email: ['', [Validators.required, Validators.pattern(/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/)]],
-    password: ['', Validators.required]
-  });
+  loginForm: FormGroup;
 
-  constructor(private fb: FormBuilder,
-              private authService: AuthService,
-              private router: Router) {
-                this.loginFormGroup.valueChanges.subscribe(() => {
-                this.disabledButton = !this.loginFormGroup.valid;
-              });}
+  constructor(private fb: FormBuilder) {
+    this.loginForm = this.fb.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', Validators.required]
+    });
+  }
 
-  login(): void {
-    this.disabledButton = true;
-    const login: LoginRequest = {
-      email: this.loginFormGroup.get('email')?.value || '',
-      password: this.loginFormGroup.get('password')?.value || '',
-    }
-    
-    if (this.loginFormGroup.valid && login) {
-      this.authService.loginService(login).subscribe({
-        next: (response: any) => {
-          const token = response.token;
-          const expirationTime = new Date().getTime() + 30000;
-
-          localStorage.setItem('token', token);
-          localStorage.setItem('expiration', expirationTime.toString());
-
-          this.authService.logoutAfterTimeoutService();
-          this.router.navigate(['/home']);
-        
-        },
-        error: (error: HttpErrorResponse) => {
-          //   Swal.fire({
-          //     icon: 'error',
-          //     title: 'Login error!',
-          //     text: 'wrong email or password',
-          // });
-          this.disabledButton = false;
-        }
-      });
+  submitForm() {
+    if (this.loginForm.valid) {
+      // Processar o envio do formulário
+      console.log(this.loginForm.value);
+    } else {
+      // Marcar os campos como tocados para mostrar os erros
+      this.loginForm.markAllAsTouched();
     }
   }
+
+
+
+
+
 }
 
